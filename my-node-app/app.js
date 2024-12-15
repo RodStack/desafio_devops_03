1a0764e2c973d7de4d22dd1261ed1b5916535fb2
Rod <fuenzalida.rodrigo@gmail.com>
2024-12-15 20:09:15 -0300

    V1.0

---
+++ my-node-app/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+  res.json({
+    mensaje: 'Hola, estoy desplegando mi primera aplicación',
+    fecha: new Date()
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en puerto ${PORT}`);
+});
